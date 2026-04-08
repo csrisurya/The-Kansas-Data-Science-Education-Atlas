c1d617cf09f8a8d1fb3f37ecd3bf09598ac56a75
Srisurya Subhang Chandramouli <srisurya@ip-10-130-48-250.gpvpn.ksu.edu>
2026-04-07 21:30:07 -0700

    Update vite base path to /~sschandr/atlas/

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/csk12/atlas/',
+  base: '/~sschandr/atlas/',
   server: {
     proxy: {
       '/api': {
